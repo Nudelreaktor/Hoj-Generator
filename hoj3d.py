fc5fe480f55be107d06b6773e7abaaf2a26edf4c
FRANZ-NOTEBOOK\Franz <nudelreaktor@yahoo.de>
2017-12-14 11:33:38 +0100

    add p_hoj

--- hoj3d.py
+++ hoj3d.py
@@ -44,6 +44,9 @@ def compute_hoj3d( list_of_joints, reference_join, reference_join_up, reference_
 	if(use_triangle_function):
 		probability_function = trinangle_function
 
+	arm_length, left_hand_sholder, right_hand_sholder = calculate_arm_length()
+	leg_length, left_foot_hip, right_foot_hip = calculate_leg_length()
+
 	#translation
 	translation_vector = np.array([-reference_join.get_WorldJoint()[0], -reference_join.get_WorldJoint()[1], -reference_join.get_WorldJoint()[2]])
 
@@ -117,10 +120,14 @@ def compute_hoj3d( list_of_joints, reference_join, reference_join_up, reference_
 	#hoj = np.array(hoj3d)
 	#print(hoj.sum())
 
+	p_hoj = []
+	p_hoj.append(hoj3d)
+	p_hoj.append([left_hand_sholder, right_hand_sholder, left_foot_hip, right_foot_hip])
+
 	t1 = time.time()
 	n_time += t1 - t0
 
-	return hoj3d,n_time
+	return np.array(p_hoj),n_time
 
 def transform_coordinate(x,y,z,vector):
 	vector = np.array(vector)
@@ -191,3 +198,63 @@ def trinangle_function(x,my):
 		p = (-x + my + 30) / 30
 
 	return max(p,0)
+
+def calculate_arm_length():
+	# calculate arm lenght
+	#			sholder								elbow
+	left_upper_arm = np.array(list_of_joints[4].get_WorldJoint()) - np.array(list_of_joints[5].get_WorldJoint())
+	#			elbow								hand
+	left_under_arm = np.array(list_of_joints[5].get_WorldJoint()) - np.array(list_of_joints[6].get_WorldJoint())
+	left_arm = ma.sqrt((left_upper_arm * left_upper_arm).sum()) + ma.sqrt((left_under_arm * left_under_arm).sum())
+
+	#			sholder								elbow
+	right_upper_arm = np.array(list_of_joints[8].get_WorldJoint()) - np.array(list_of_joints[9].get_WorldJoint())
+	#			elbow								hand
+	right_under_arm = np.array(list_of_joints[9].get_WorldJoint()) - np.array(list_of_joints[10].get_WorldJoint())
+	right_arm = ma.sqrt((right_upper_arm * right_upper_arm).sum()) + ma.sqrt((right_under_arm * right_under_arm).sum())
+	arm_length = ((left_arm + right_arm) / 2)
+
+	# calculate distance sholder -> hand
+	#			sholder								hand
+	left_vector = np.array(list_of_joints[4].get_WorldJoint()) - np.array(list_of_joints[6].get_WorldJoint())
+	distance_left = ma.sqrt((left_vector * left_vector).sum())
+	# nomalize
+	left = distance_left / arm_length
+
+	#			sholder								hand
+	right_vector = np.array(list_of_joints[8].get_WorldJoint()) - np.array(list_of_joints[10].get_WorldJoint())
+	distance_right = ma.sqrt((right_vector * right_vector).sum())
+	# normalize
+	right = distance_right / arm_length
+
+	return arm_length, left, right
+
+def calculate_leg_length:
+	# calculate leg lenght
+	#			hip								knee
+	left_upper_leg = np.array(list_of_joints[12].get_WorldJoint()) - np.array(list_of_joints[13].get_WorldJoint())
+	#			knee								foot
+	left_under_leg = np.array(list_of_joints[13].get_WorldJoint()) - np.array(list_of_joints[14].get_WorldJoint())
+	left_leg = ma.sqrt((left_upper_leg * left_upper_leg).sum()) + ma.sqrt((left_under_leg * left_under_leg).sum())
+
+	#			hip								knee
+	right_upper_leg = np.array(list_of_joints[16].get_WorldJoint()) - np.array(list_of_joints[17].get_WorldJoint())
+	#			knee								foot
+	right_under_leg = np.array(list_of_joints[17].get_WorldJoint()) - np.array(list_of_joints[18].get_WorldJoint())
+	right_leg = ma.sqrt((right_upper_leg * right_upper_leg).sum()) + ma.sqrt((right_under_leg * right_under_leg).sum())
+	leg_lenght = ((left_leg + right_leg) / 2)
+
+	# calculate distance hip -> foot
+	#			hip								foot
+	left_vector = np.array(list_of_joints[12].get_WorldJoint()) - np.array(list_of_joints[14].get_WorldJoint())
+	distance_left = ma.sqrt((left_vector * left_vector).sum())
+	# nomalize
+	left = distance_left / arm_length
+
+	#			hip								foot
+	right_vector = np.array(list_of_joints[16].get_WorldJoint()) - np.array(list_of_joints[18].get_WorldJoint())
+	distance_right = ma.sqrt((right_vector * right_vector).sum())
+	# normalize
+	right = distance_right / arm_length
+
+	return leg_lenght, left, right
